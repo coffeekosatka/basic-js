1dc5a7da808e63d42a95ff2e442eed9faf3ce66c
coffeekosatka <viikkuullyy@gmail.com>
2023-12-24 00:01:25 +0300

    feat: js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function dateSample(sampleActivity) {
+  //age = ln(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)
+  if(typeof sampleActivity === "string") {
+    const age = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+    if(age > 0 && age !== Infinity){
+      return age;
+    }
+    else {
+      return false;
+    }
+  }
+  else {
+      return false;
+    } 
+  }
+
 
 module.exports = {
   dateSample
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,10 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let stringData = n.toString();
+  let result = 0;
+
+  for (let i = 0; i < stringData.length; i++) {
+    let newNumber = parseInt(stringData.replace(stringData[i].toString(), ''));
+    result = Math.max(result, newNumber);
+  }
+
+  return result;
 }
+
+
 
 module.exports = {
   deleteDigit
