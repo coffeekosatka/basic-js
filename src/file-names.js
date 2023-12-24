ed20f369694cb017259edfb027a5d6d1bb20a1df
coffeekosatka <viikkuullyy@gmail.com>
2023-12-24 23:16:00 +0300

    feat: js

--- src/file-names.js
+++ src/file-names.js
@@ -15,10 +15,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const result = [];
+
+  names.forEach((name) => {
+    let count = 0;
+    let newName = name;
+
+    while (result.includes(newName)) {
+      count++;
+      newName = `${name}(${count})`;
+    }
+
+    result.push(newName);
+  });
+
+  return result;
 }
+
 
 module.exports = {
   renameFiles
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,10 +12,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let numString = n.toString();
+
+  while (numString.length > 1) {
+    let digitSum = 0;
+    
+    for (let digit of numString) {
+      digitSum += Number(digit);
+    }
+    
+    numString = digitSum.toString();
+  }
+
+  return Number(numString);
 }
+
 
 module.exports = {
   getSumOfDigits
